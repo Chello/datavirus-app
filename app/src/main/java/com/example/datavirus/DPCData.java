36cc974cd032172fc95c95493b372f5fa05ac5b4
Federico Rachelli <frakelli@gmail.com>
2020-04-24 00:04:55 +0200

    Correctly obtaining and ordering data

--- app/src/main/java/com/example/datavirus/DPCData.java
+++ app/src/main/java/com/example/datavirus/DPCData.java
@@ -4,7 +4,9 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 
 /**
  * Objects from this class will contain data from Nazionale, Regionale and Provinciale sections.
@@ -20,25 +22,80 @@ public class DPCData {
     private Gson gson;
     //Reports for territorial data
     private DailyReport[] nazionale;
-    private DailyReport[] regionale;
-    private DailyReport[] provinciale;
+    private HashMap<String, ArrayList<DailyReport>> regionale;
+    private HashMap<String, ArrayList<DailyReport>> provinciale;
 
+    /**
+     * Returns the Nazionale report
+     * @return the Nazionale report
+     */
     public DailyReport[] getNazionale() {
         return this.nazionale;
+    }
+
+    /**
+     * Returns the Regionale report, by giving it's regione
+     * @param regione the regione to obtain
+     * @return the Regiona report
+     */
+    public DailyReport[] getRegionaleReport(String regione) {
+        DailyReport[] arr = new DailyReport[this.regionale.size()];
+        if (this.regionale.containsKey(regione))
+            return this.regionale.get(regione).toArray(arr);
+        return null;
+    }
+
+    /**
+     * Returns the Provinciale report, by giving it's provincia
+     * @param provincia the provincia to obtain
+     * @return the Provincia report
+     */
+    public DailyReport[] getProvincialeReport(String provincia) {
+        DailyReport[] arr = new DailyReport[this.provinciale.size()];
+        if (this.provinciale.containsKey(provincia))
+            return this.provinciale.get(provincia).toArray(arr);
+        return null;
     }
 
     public DPCData(String nazionaleJson, String regionaleJson, String provincialeJson) {
         this.gson = new Gson();
 
         this.nazionale = gson.fromJson(nazionaleJson, DailyReport[].class);
-        this.regionale = gson.fromJson(regionaleJson, DailyReport[].class);
-        this.provinciale = gson.fromJson(provincialeJson, DailyReport[].class);
-        Log.d("DPCParse", "Here we are");
+        DailyReport[] regionale = gson.fromJson(regionaleJson, DailyReport[].class);
+        DailyReport[] provinciale = gson.fromJson(provincialeJson, DailyReport[].class);
+
+        this.regionale = createKeyValueList(regionale);
+        this.provinciale = createKeyValueList(provinciale);
+
+        Log.d("DPCParse", "Data parsed");
+    }
+
+    /**
+     * Creates the key-value Hash Table, useful for organizing data into field of interest.
+     * The field used is DPCData's 'denominazione', obtained via getDenominazione()
+     * @param reports the row array to order
+     * @return an ordered Hash Table by field ('denominazione')
+     */
+    private HashMap<String, ArrayList<DailyReport>> createKeyValueList(DailyReport[] reports) {
+        //This HashMap contains the ArrayList
+        HashMap<String, ArrayList<DailyReport>> build = new HashMap<String, ArrayList<DailyReport>>();
+        //Build the reports, by field
+        for (DailyReport report : reports) {
+            String currentDenominazione = report.getDenominazione();
+            //if the array does not yet contains the current 'denominazione'
+            if (!build.containsKey(currentDenominazione))
+                //create it
+                build.put(currentDenominazione, new ArrayList<DailyReport>());
+            //put the current report into the hashmap in the right array
+            build.get(currentDenominazione).add(report);
+        }
+        return build;
     }
 
     public class DailyReport {
-
-        //private Date data;
+        private String data;
+        private String denominazione_regione;
+        private String denominazione_provincia;
         private Integer ricoverati_con_sintomi;
         private Integer terapia_intensiva;
         private Integer isolamento_domiciliare;
@@ -50,6 +107,10 @@ public class DPCData {
         private Integer totale_casi;
         private Integer tamponi;
         private Integer casi_testati;
+
+        public String getDenominazione() {
+            return (this.denominazione_provincia != null) ? this.denominazione_provincia : this.denominazione_regione;
+        }
 
         public Integer getRicoverati_con_sintomi() {
             return ricoverati_con_sintomi;
@@ -95,19 +156,19 @@ public class DPCData {
             return casi_testati;
         }
 
-        public DailyReport(Integer ricoverati_con_sintomi, Integer terapia_intensiva, Integer isolamento_domiciliare, Integer totale_positivi, Integer variazione_totale_positivi, Integer nuovi_positivi, Integer dimessi_guariti, Integer deceduti, Integer totale_casi, Integer tamponi, Integer casi_testati) {
-            this.ricoverati_con_sintomi = ricoverati_con_sintomi;
-            this.terapia_intensiva = terapia_intensiva;
-            this.isolamento_domiciliare = isolamento_domiciliare;
-            this.totale_positivi = totale_positivi;
-            this.variazione_totale_positivi = variazione_totale_positivi;
-            this.nuovi_positivi = nuovi_positivi;
-            this.dimessi_guariti = dimessi_guariti;
-            this.deceduti = deceduti;
-            this.totale_casi = totale_casi;
-            this.tamponi = tamponi;
-            this.casi_testati = casi_testati;
-        }
+//        public DailyReport(String denominazione_regione, String denominazione_provincia, Integer ricoverati_con_sintomi, Integer terapia_intensiva, Integer isolamento_domiciliare, Integer totale_positivi, Integer variazione_totale_positivi, Integer nuovi_positivi, Integer dimessi_guariti, Integer deceduti, Integer totale_casi, Integer tamponi, Integer casi_testati) {
+//            this.ricoverati_con_sintomi = ricoverati_con_sintomi;
+//            this.terapia_intensiva = terapia_intensiva;
+//            this.isolamento_domiciliare = isolamento_domiciliare;
+//            this.totale_positivi = totale_positivi;
+//            this.variazione_totale_positivi = variazione_totale_positivi;
+//            this.nuovi_positivi = nuovi_positivi;
+//            this.dimessi_guariti = dimessi_guariti;
+//            this.deceduti = deceduti;
+//            this.totale_casi = totale_casi;
+//            this.tamponi = tamponi;
+//            this.casi_testati = casi_testati;
+//        }
     }
 
 }
--- app/src/main/java/com/example/datavirus/DataTilesFragment.java
+++ app/src/main/java/com/example/datavirus/DataTilesFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,6 +29,7 @@ public class DataTilesFragment extends Fragment implements UpdateUI {
     private String mParam2;
 
     private DataParser parser;
+    private DPCData covidData;
 
     public DataTilesFragment() {
         // Required empty public constructor
@@ -69,7 +71,7 @@ public class DataTilesFragment extends Fragment implements UpdateUI {
         return v;
     }
 
-    private void setRegionalSpinner() {
+    private void setRegionaleSpinner() {
 
     }
 
@@ -84,7 +86,9 @@ public class DataTilesFragment extends Fragment implements UpdateUI {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Spinner regional = (Spinner) v.findViewById(R.id.spinner_regional);
-                if (!parent.getItemAtPosition(position).toString().equals("Nazionale")) {
+
+                String selected = parent.getItemAtPosition(position).toString();
+                if (!selected.equals("Nazionale")) {
                     regional.setVisibility(View.VISIBLE);
                 } else {
                     regional.setVisibility(View.INVISIBLE);
@@ -98,40 +102,60 @@ public class DataTilesFragment extends Fragment implements UpdateUI {
         });
     }
 
-    @Override
-    public void updateData(DPCData data) {
-        DPCData.DailyReport[] report = data.getNazionale();
-
+    /**
+     * Update tiles, using data passed.
+     * It only uses the last two days for getting the current data and the delta
+     * @param report report to use for the update
+     */
+    private void updateTiles(DPCData.DailyReport[] report) {
         //Set total value of last day
         TextView total = (TextView) getView().findViewById(R.id.tile_total);
         total.setText(report[report.length -1].getTotale_casi().toString());
         //Set total delta of last day
         TextView totalDelta = (TextView) getView().findViewById(R.id.tile_total_delta);
-        totalDelta.setText("Δ" + (report[report.length -1].getTotale_casi() - report[report.length-2].getTotale_casi()));
+        totalDelta.setText("Δ " + (report[report.length -1].getTotale_casi() - report[report.length-2].getTotale_casi()));
 
         //Set active value of last day
         TextView active = (TextView) getView().findViewById(R.id.tile_active);
         active.setText(report[report.length -1].getTotale_positivi().toString());
         //Set active delta of last day
         TextView activeDelta = (TextView) getView().findViewById(R.id.tile_active_delta);
-        activeDelta.setText("Δ" + (report[report.length -1].getTotale_positivi() - report[report.length-2].getTotale_positivi()));
+        activeDelta.setText("Δ " + (report[report.length -1].getTotale_positivi() - report[report.length-2].getTotale_positivi()));
 
         //Set healed value of last day
         TextView healed = (TextView) getView().findViewById(R.id.tile_healed);
         healed.setText(report[report.length -1].getDimessi_guariti().toString());
         //Set healed delta of last day
         TextView healedDelta = (TextView) getView().findViewById(R.id.tile_healed_delta);
-        healedDelta.setText("Δ" + (report[report.length -1].getDimessi_guariti() - report[report.length-2].getDimessi_guariti()));
+        healedDelta.setText("Δ " + (report[report.length -1].getDimessi_guariti() - report[report.length-2].getDimessi_guariti()));
 
         //Set deaths value of last day
         TextView deaths = (TextView) getView().findViewById(R.id.tile_deaths);
         deaths.setText(report[report.length -1].getDeceduti().toString());
         //Set deaths delta of last day
         TextView deathsDelta = (TextView) getView().findViewById(R.id.tile_deaths_delta);
-        deathsDelta.setText("Δ" + (report[report.length -1].getDeceduti() - report[report.length-2].getDeceduti()));
-
+        deathsDelta.setText("Δ " + (report[report.length -1].getDeceduti() - report[report.length-2].getDeceduti()));
     }
 
+    /**
+     * Updates data given by the DataParser object.
+     * This method will be called only after the data are obtained
+     * @param data the data to use for updates
+     */
+    @Override
+    public void updateData(DPCData data) {
+        this.covidData = data;
+
+        DPCData.DailyReport[] regionali = this.covidData.getRegionaleReport("Emilia-Romagna");
+        Log.d("DataTilesFragment", "Here I am");
+        //Update the daily national report
+        this.updateTiles(data.getNazionale());
+    }
+
+    /**
+     * Update the parser. Called by the parser.
+     * @param parser
+     */
     @Override
     public void setParser(DataParser parser) {
         this.parser = parser;
