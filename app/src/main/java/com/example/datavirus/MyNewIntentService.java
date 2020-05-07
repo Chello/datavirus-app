a56025d19532e3fe9232c7c724a50add4bc02cd7
Federico Rachelli <frakelli@gmail.com>
2020-05-07 19:49:56 +0200

    Notifications works!

--- app/src/main/java/com/example/datavirus/MyNewIntentService.java
+++ app/src/main/java/com/example/datavirus/MyNewIntentService.java
@@ -21,7 +21,7 @@ public class MyNewIntentService extends IntentService {
         builder.setContentText("This is the Body");
         builder.setSmallIcon(R.drawable.ic_launcher_background);
         Intent notifyIntent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 2, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 2, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         //to be able to launch your activity from the notification
         builder.setContentIntent(pendingIntent);
         Notification notificationCompat = builder.build();
